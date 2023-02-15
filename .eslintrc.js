a6238df9c707fc6852f379f5d36c84df47c75e1d
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-15 21:08:00 +0100

    Fix indent spaces on all files to pass audit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,18 @@
 module.exports = {
-	env: {
-		commonjs: true,
-		es2021: true,
-		node: true,
-	},
-	extends: 'xo',
-	overrides: [
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
-	rules: {
-	},
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "xo",
+  overrides: [
+    {
+      extends: ["xo-typescript"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
 };
