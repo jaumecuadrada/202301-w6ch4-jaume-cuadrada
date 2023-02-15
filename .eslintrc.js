01abe9d31176732a300bfb3df28afc9c5d43ee64
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-02-15 21:11:20 +0100

    Minor fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   env: {
-    commonjs: true,
     es2021: true,
     node: true,
   },
-  extends: "xo",
+  extends: ["xo", "prettier"],
   overrides: [
     {
-      extends: ["xo-typescript"],
+      extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
     },
   ],
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {},
 };
